cdf6ed64239fd4419154f1bdef63591f7bf23f00
Unella Jyzene <ujyzene@oa-j6.21-school.ru>
2019-10-03 15:29:57 +0300

    libft: gnl fix - MAX_FD too big

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ujyzene <ujyzene@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 12:29:05 by ujyzene           #+#    #+#             */
-/*   Updated: 2019/09/22 15:14:01 by ujyzene          ###   ########.fr       */
+/*   Updated: 2019/10/03 15:28:52 by ujyzene          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 # include <libft.h>
 # define BUFF_SIZE 32
-# define MAX_FD 2147483647
+# define MAX_FD 10
 # define OK 1
 # define ERROR -1
 # define EXIT 0
